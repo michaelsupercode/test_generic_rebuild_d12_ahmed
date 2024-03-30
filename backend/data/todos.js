d23890628a752991cece99311d6a2c382c48b57b
Michael Hanel <michaelsupercode@gmail.com>
2024-03-30 14:01:42 +0100

    set

---
+++ backend/data/todos.js
@@ -0,0 +1,6 @@
+[
+    {"_id":{"$oid":"627d45b279a28a108a077f26"},"title":"..gesund werden..","completed":false},
+    {"_id":{"$oid":"627d45b279a28a108a077f26"},"title":"..gesund werden..","completed":false},
+    {"_id":{"$oid":"627d45b279a28a108a077f26"},"title":"..gesund werden..","completed":false},
+    
+]
